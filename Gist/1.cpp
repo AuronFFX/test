0553723578389705a5ab28dc285e12aa5c7e27db
AuronFFX <Terran.ITX@gmail.com>
2015-01-03 00:37:39 +0900

    1

--- Gist/1.cpp
+++ Gist/1.cpp
@@ -1,1 +1,16 @@
 #include <iostream>
+#include <opencv2\opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+void main()
+{
+	Mat gabor = getGaborKernel(Size(64, 64), 20, 1, 20, 1, 0);
+	Mat gray,img = imread("1.jpg");
+	resize(img, img, Size(64, 64));
+	cvtColor(img, gray, CV_RGB2GRAY);
+	//filter2D()
+	imshow("1", img);
+	waitKey();
+}
