5903d82afc5c6f2b16e3446970dd934fb9a84af8
AuronFFX <Terran.ITX@gmail.com>
2015-01-05 02:46:03 +0900

    2015/5/12 - 2:45 - Home

--- Gist/1.cpp
+++ Gist/1.cpp
@@ -6,11 +6,28 @@ using namespace cv;
 
 void main()
 {
+	double res=0;
 	Mat gabor = getGaborKernel(Size(64, 64), 20, 1, 20, 1, 0);
+	imshow("Filter", gabor);
 	Mat gray,img = imread("1.jpg");
 	resize(img, img, Size(64, 64));
-	cvtColor(img, gray, CV_RGB2GRAY);
-	//filter2D()
-	imshow("1", img);
+	cvtColor(img, gray, COLOR_BGR2GRAY); 
+	imshow("1", gray);
+	unsigned char* img_data;
+	double* gabor_data;
+	gabor_data = (double*)gabor.data;
+	img_data = gray.data;
+	int start_time = getTickCount();
+	for (int k = 0; k < 1024; k++)
+	for (int i = 0; i < 65 * 65; i++)
+	{
+		res += gabor_data[i] * (float)img_data[i];
+		//cout << (float)img_data[i] << endl;
+	}
+	int end_time= getTickCount();
+	double elapsed_time = (end_time - start_time) / getTickFrequency();
+	cout << res << endl;
+	cout << elapsed_time << endl;
+	//imshow("2", res);
 	waitKey();
 }
